46936d5020f0a16a06fe0a0e74aa8cfa1db69989
Alexandru Voica <alexandru.voica16@gmail.com>
2018-05-23 20:57:26 +0300

    Implemented list of photos with working fetch

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ class CatPhotoElement {
     this.photo = photo;
     this.name = name;
     this.counter = counter;
+    this.isVisible = false;
   }
 
-  increaseCounter () {
+  incrementCounter () {
     this.counter ++;
   }
 
@@ -26,63 +27,62 @@ class CatPhotoElement {
   }
 }
 
-window.onload = function () {
-  const globalContainer = document.querySelector('.global-container');
-  globalContainer.innerHTML = '';
+const globalContainer = document.querySelector('.global-container');
+const listContainer = document.querySelector('.list-container');
+globalContainer.innerHTML = '';
+window.URL = window.URL || window.webkitURL;
 
-  let cats = [];
+let cats = [];
+
+async function createList () {
   const numberOfCats = 9;
 
   for (let index = 0; index < numberOfCats; index ++) {
-    // globalContainer.innerHTML += templateElement
+    let photoUrl = '';
+    let fetchPromise = fetchCatPhoto();
+    await fetchPromise.then(link => {
+      photoUrl = link;
+    });
 
-    cats.push(new CatPhotoElement(fetchCatPhoto(), generateName(), 0));
+    cats.push(new CatPhotoElement(photoUrl, generateName(), 0));
 
     let container = document.createElement('div');
     container.classList.add('container');
     container.innerHTML = '';
-    populate(container);
-    globalContainer.appendChild(container);
+    container.innerHTML += cats[index].renderPhoto();
+    listContainer.appendChild(container);
   }
+}
 
-  // async function populate (container) {
-  //   await addPhoto(container);
-  //   addCounter(container);
-  //   listen(container);
-  // }
+createList();
 
-  function fetchCatPhoto () {
-    const catApiUrl = 'https://cataas.com/cat';
-    return fetch(catApiUrl)
-            .then(response => response.blob())
-            .then(blob => {
-              let link = URL.createObjectURL(blob);
-              return link;
-              // let img = `<img src="${link}" alt="cute kitty">`;
-              // let newElement = `<div class="photo-container">${img}</div>`;
-              // container.innerHTML += newElement;
-            });
-  }
+// async function populate (container) {
+//   await addPhoto(container);
+//   addCounter(container);
+//   listen(container);
+// }
 
-  function addCounter (container) {
-    let info = `<h6 class="name">${generateName()}</h6>
-                <h1 class="counter">0</h1>
-                <h6>times clicked</h6>`;
-    let newElement = `<div class="counter-container">${info}</div>`;
-    container.innerHTML += newElement;
-  }
+function fetchCatPhoto () {
+  const catApiUrl = 'https://cataas.com/cat';
+  return fetch(catApiUrl)
+  .then(response => response.blob())
+  .then(blob => {
+    return window.URL.createObjectURL(blob);
+    // return link;
+    // let img = `<img src="${link}" alt="cute kitty">`;
+    // let newElement = `<div class="photo-container">${img}</div>`;
+    // container.innerHTML += newElement;
+  });
+}
 
-  function listen (container) {
-    const photoContainer = container.querySelector('.photo-container');
-    const counterContainer = container.querySelector('.counter-container');
-    photoContainer.addEventListener('click', function(event) {
-      event.preventDefault();
-      const counter = counterContainer.querySelector('.counter');
-      let counterValue = counter.textContent;
-      counterValue ++;
-      counter.textContent = counterValue;
-    });
-  }
-
-
-};
+// function listen (container) {
+//   const photoContainer = container.querySelector('.photo-container');
+//   const counterContainer = container.querySelector('.counter-container');
+//   photoContainer.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const counter = counterContainer.querySelector('.counter');
+//     let counterValue = counter.textContent;
+//     counterValue ++;
+//     counter.textContent = counterValue;
+//   });
+// }
